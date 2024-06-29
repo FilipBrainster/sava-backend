454017af541f43c70d6eba4165aba791f06a130c
Filip <kuzmanovska.a23092@next.edu.mk>
2024-06-29 11:49:55 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+
+Route::get('/policies', function () {
+    return [
+        'data' => true
+    ];
+});
+
+require __DIR__.'/auth.php';
